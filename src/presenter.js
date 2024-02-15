be16951d147e4f8050a229e304da8067bf1132dd
Jonathan <jonathan.moruno@ucb.edu.bo>
2024-02-15 10:13:58 -0400

    saludando tomano el cuenta la edad

--- src/presenter.js
+++ src/presenter.js
@@ -1,6 +1,9 @@
 import saludar from "./saludar";
 
 const nombre_input = document.querySelector("#nombre");
+const edad_input = document.querySelector("#edad");
+const genero_input = document.querySelector("#genero");
+const idioma_input = document.querySelector("#idioma");
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
 
@@ -8,9 +11,20 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const nombre = nombre_input.value;
- 
-
-  div.innerHTML = "<p> Hola " + saludar(nombre) + "</p>";
+  const edad = edad_input.value;
+  const genero = genero_input.value;
+  const idioma = idioma_input.value;
+  
+    if (edad>=30) {
+      
+      div.innerHTML = "<p> Hola senor " + saludar(nombre) + "</p>";
+    }  
+    else {
+      div.innerHTML = "<p> Hola " + saludar(nombre) + "</p>";
+    } 
+  
+  
+  
 });
 
 
