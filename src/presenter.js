804de02ea32f8180fc92e700a9165387be314921
Jonathan <jonathan.moruno@ucb.edu.bo>
2024-02-20 22:31:48 +0700

    refactorizacion del codigo anterior

--- src/presenter.js
+++ src/presenter.js
@@ -1,12 +1,11 @@
-import saludar from "./saludar";
-
+import funcionesSaludo from "./saludar";
 const nombre_input = document.querySelector("#nombre");
 const edad_input = document.querySelector("#edad");
 const genero_input = document.querySelector("#genero");
 const idioma_input = document.querySelector("#idioma");
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
-// const div3 = document.querySelector("#resultado-div3");
+
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -15,91 +14,19 @@ form.addEventListener("submit", (event) => {
   const edad = edad_input.value;
   const genero = genero_input.value;
   const idioma = idioma_input.value;
-  console.log(genero)
-  // let fechaHoraActual = new Date();
-  // let opciones = { hour: '2-digit', minute: '2-digit', hour12: false };
-  // let soloHoraS = fechaHoraActual.toLocaleTimeString(undefined, opciones).split(":")[0];
-  // let soloHora = parseInt(soloHoraS);
+  
   let fechaActual = new Date();
   let soloHora = fechaActual.getHours();
-  console.log(soloHora)
-  if ((soloHora >= 18 && soloHora <=23) ) {
-    if (idioma === 'Esp') {
-      if (edad >= 30) {
-        if (genero === 'M') {
-          div.innerHTML = "<p> Buenas noches senor " + saludar(nombre) + "</p>";
-        } else if (genero === "F") {
-          div.innerHTML = "<p> Buenas noches senora " + saludar(nombre) + "</p>";
-        }
-      } else {
-        div.innerHTML = "<p> Buenas noches " + saludar(nombre) + "</p>";
-      }
-    } else if (idioma === 'Eng') {
-      if (edad >= 30) {
-        if (genero === 'M') {
-          div.innerHTML = "<p> Good evening mister " + saludar(nombre) + "</p>";
-        } else if (genero === "F") {
-          div.innerHTML = "<p> Good evening miss " + saludar(nombre) + "</p>";
-        }
-      } else {
-        div.innerHTML = "<p> Good evening " + saludar(nombre) + "</p>";
-      }
-    }
-  } else if (soloHora >= 0 && soloHora <=11) {
-    if (idioma === 'Esp') {
-      if (edad >= 30) {
-        if (genero === 'M') {
-          div.innerHTML = "<p> Buenos dias senor " + saludar(nombre) + "</p>";
-        } else if (genero === "F") {
-          div.innerHTML = "<p> Buenos dias senora " + saludar(nombre) + "</p>";
-        }
-      } else {
-        div.innerHTML = "<p> Buenos dias " + saludar(nombre) + "</p>";
-      }
-    } else if (idioma === 'Eng') {
-      if (edad >= 30) {
-        if (genero === 'M') {
-          div.innerHTML = "<p> Good morning mister " + saludar(nombre) + "</p>";
-        } else if (genero === "F") {
-          div.innerHTML = "<p> Good morning miss " + saludar(nombre) + "</p>";
-        }
-      } else {
-        div.innerHTML = "<p> Good morning " + saludar(nombre) + "</p>";
-      }
-    }
-  } else if (soloHora >= 12 && soloHora <= 17) {
-    if (idioma === 'Esp') {
-      if (edad >= 30) {
-        if (genero === 'M') {
-          div.innerHTML = "<p> Buenas tardes senor " + saludar(nombre) + "</p>";
-        } else if (genero === "F") {
-          div.innerHTML = "<p> Buenas tardes senora " + saludar(nombre) + "</p>";
-        }
-      } else {
-        div.innerHTML = "<p> Buenas tardes " + saludar(nombre) + "</p>";
-      }
-    } else if (idioma === 'Eng') {
-      if (edad >= 30) {
-        if (genero === 'M') {
-          div.innerHTML = "<p> Good afternoon mister " + saludar(nombre) + "</p>";
-        } else if (genero === "F") {
-          div.innerHTML = "<p> Good afternoon miss " + saludar(nombre) + "</p>";
-        }
-      } else {
-        div.innerHTML = "<p> Good afternoon " + saludar(nombre) + "</p>";
-      }
-    }
-  }
-  
-
-  
-  // div3.innerHTML = "<p>" + soloHora + "</p>";
+ 
+  div.innerHTML = "<p>" + funcionesSaludo.retornarSaludoAcordeHoraIdioma(soloHora, idioma) + funcionesSaludo.retornarGeneroIdioma(genero, idioma, edad) + funcionesSaludo.saludar(nombre) + "</p>";
 
 
   
   
   
 });
+
+
 
 
 
--- src/saludar.js
+++ src/saludar.js
@@ -1,5 +1,170 @@
 function saludar(nombre) {
   return nombre;
 }
+function retornarGeneroIdioma(genero,idioma,edad) {
+  if(idioma=="Esp")
+  {
+    if (genero=="M")
+    {
+      if(edad>=30)
+      {
+        return " señor "
+      }
+      else if(edad<30)
+      {
+        return ""
+      }
+    }
+    else if(genero=="F")
+    {
+      if(edad>=30)
+      {
+        return " señora "
+      }
+      else if(edad<30)
+      {
+        return ""
+      }
+    }
 
-export default saludar;
+  }
+  else if(idioma=="Eng")
+  {
+    if (genero=="M")
+    {
+      if(edad>=30)
+      {
+        return " mister "
+      }
+      else if(edad<30)
+      {
+        return ""
+      }
+    }
+    else if(genero=="F")
+    {
+      if(edad>=30)
+      {
+        return " miss "
+      }
+      else if(edad<30)
+      {
+        return ""
+      }
+    }
+  }
+}
+function retornarSaludoAcordeHoraIdioma(soloHora,idioma)
+{
+  if ((soloHora >= 18 && soloHora <=23) )
+  {
+    if (idioma=="Esp")
+    {
+      return "Buenas noches "
+
+    }
+    else if(idioma=="Eng")
+    {
+      return "Good evening "
+    }
+  }
+  else if (soloHora >= 0 && soloHora <=11) 
+  {
+    if (idioma=="Esp")
+    {
+      return "Buenos dias "
+
+    }
+    else if(idioma=="Eng")
+    {
+      return "Good morning "
+    }
+  }
+  else if (soloHora >= 12 && soloHora <= 17)
+  {
+    if (idioma=="Esp")
+    {
+      return "Buenas tardes "
+
+    }
+    else if(idioma=="Eng")
+    {
+      return "Good afternoon "
+    }
+  }
+
+
+}
+
+
+export default {saludar,retornarGeneroIdioma,retornarSaludoAcordeHoraIdioma};
+
+// function saludoInicial(edad,genero,idioma,soloHora){
+//   if ((soloHora >= 18 && soloHora <=23) ) {
+//     if (idioma === 'Esp') {
+//       if (edad >= 30) {
+//         if (genero === 'M') {
+//           div.innerHTML = "<p> Buenas noches senor " + saludar(nombre) + "</p>";
+//         } else if (genero === "F") {
+//           div.innerHTML = "<p> Buenas noches senora " + saludar(nombre) + "</p>";
+//         }
+//       } else {
+//         div.innerHTML = "<p> Buenas noches " + saludar(nombre) + "</p>";
+//       }
+//     } else if (idioma === 'Eng') {
+//       if (edad >= 30) {
+//         if (genero === 'M') {
+//           div.innerHTML = "<p> Good evening mister " + saludar(nombre) + "</p>";
+//         } else if (genero === "F") {
+//           div.innerHTML = "<p> Good evening miss " + saludar(nombre) + "</p>";
+//         }
+//       } else {
+//         div.innerHTML = "<p> Good evening " + saludar(nombre) + "</p>";
+//       }
+//     }
+//   } else if (soloHora >= 0 && soloHora <=11) {
+//     if (idioma === 'Esp') {
+//       if (edad >= 30) {
+//         if (genero === 'M') {
+//           div.innerHTML = "<p> Buenos dias senor " + saludar(nombre) + "</p>";
+//         } else if (genero === "F") {
+//           div.innerHTML = "<p> Buenos dias senora " + saludar(nombre) + "</p>";
+//         }
+//       } else {
+//         div.innerHTML = "<p> Buenos dias " + saludar(nombre) + "</p>";
+//       }
+//     } else if (idioma === 'Eng') {
+//       if (edad >= 30) {
+//         if (genero === 'M') {
+//           div.innerHTML = "<p> Good morning mister " + saludar(nombre) + "</p>";
+//         } else if (genero === "F") {
+//           div.innerHTML = "<p> Good morning miss " + saludar(nombre) + "</p>";
+//         }
+//       } else {
+//         div.innerHTML = "<p> Good morning " + saludar(nombre) + "</p>";
+//       }
+//     }
+//   } else if (soloHora >= 12 && soloHora <= 17) {
+//     if (idioma === 'Esp') {
+//       if (edad >= 30) {
+//         if (genero === 'M') {
+//           div.innerHTML = "<p> Buenas tardes senor " + saludar(nombre) + "</p>";
+//         } else if (genero === "F") {
+//           div.innerHTML = "<p> Buenas tardes senora " + saludar(nombre) + "</p>";
+//         }
+//       } else {
+//         div.innerHTML = "<p> Buenas tardes " + saludar(nombre) + "</p>";
+//       }
+//     } else if (idioma === 'Eng') {
+//       if (edad >= 30) {
+//         if (genero === 'M') {
+//           div.innerHTML = "<p> Good afternoon mister " + saludar(nombre) + "</p>";
+//         } else if (genero === "F") {
+//           div.innerHTML = "<p> Good afternoon miss " + saludar(nombre) + "</p>";
+//         }
+//       } else {
+//         div.innerHTML = "<p> Good afternoon " + saludar(nombre) + "</p>";
+//       }
+//     }
+//   }
+// }
